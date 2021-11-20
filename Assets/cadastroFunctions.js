3ff6c4ceb7de627573f4a0422dec739dd7e774f0
A.G.R. Ribeiro <agrribeiro12@gmail.com>
2021-11-20 17:51:36 -0300

    cadastro completo

---
+++ Assets/cadastroFunctions.js
@@ -0,0 +1,26 @@
+
+
+function limparErros(){
+    var vetorElementos = ["nome", "sobrenome", "login", "senha", "senhaConfirmar", "email"];
+
+    for(var i=0; i<vetorElementos.length; i++){
+        document.getElementById(vetorElementos[i]).style.borderColor =null;
+        document.getElementById(vetorElementos[i]).style.backgroundColor=null;
+    }
+
+}
+
+function verificarCamposVazios(){
+    limparErros()
+    var vetorElementos = ["nome", "sobrenome", "login", "senha", "senhaConfirmar", "email"];
+
+
+    for(var i=0; i<vetorElementos.length; i++){
+        if(document.getElementById(vetorElementos[i]).value==""){
+            document.getElementById(vetorElementos[i]).style.backgroundColor="pink";
+            document.getElementById(vetorElementos[i]).style.borderColor="var(--errorInput)";
+        }
+
+    }
+
+}
