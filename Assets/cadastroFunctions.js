022b03b3310913cef463986ba202fd3b2edca241
A.G.R. Ribeiro <agrribeiro12@gmail.com>
2021-11-20 18:13:10 -0300

    cadastro v2

--- Assets/cadastroFunctions.js
+++ Assets/cadastroFunctions.js
@@ -4,6 +4,7 @@ function limparErros(){
     var vetorElementos = ["nome", "sobrenome", "login", "senha", "senhaConfirmar", "email"];
 
     for(var i=0; i<vetorElementos.length; i++){
+        document.getElementById("divErro").style.display="none";
         document.getElementById(vetorElementos[i]).style.borderColor =null;
         document.getElementById(vetorElementos[i]).style.backgroundColor=null;
     }
@@ -17,6 +18,7 @@ function verificarCamposVazios(){
 
     for(var i=0; i<vetorElementos.length; i++){
         if(document.getElementById(vetorElementos[i]).value==""){
+            document.getElementById("divErro").style.display="block";
             document.getElementById(vetorElementos[i]).style.backgroundColor="pink";
             document.getElementById(vetorElementos[i]).style.borderColor="var(--errorInput)";
         }
